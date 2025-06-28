08145bd335271bcb120c096a5ae56dfa88322aed
zhangfulin <1148112937@qq.com>
2025-06-28 20:57:13 +0800

    [eable cctv_c(cctv国内)]“

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from data_source.cctv import get_cctv_news
+from data_source.cctv_c import get_cctv_c_news
 import time
 import json
     
 def process_news_website(website_name, url):
     if website_name == "CCTV":
             return get_cctv_news(website_name, url)
+    elif website_name == "CCTV_C":
+            return get_cctv_c_news(website_name, url)
     else:
         return None
 
